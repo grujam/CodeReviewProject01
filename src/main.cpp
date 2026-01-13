48fc127d5042d6f596158d118a36e91a8fa9e7b7
grujam <jseo7913@naver.com>
2026-01-13 17:42:39 +0900

    Reapply "random code."
    This reverts commit a49f616654a82c6e784fe2372dc0ac8ef7a96a21.

---
+++ src/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <async>
+#include <stdio.h>
+#include <vector>
+#include <queue>
+#include <chrono>
+#include <math.h>
+
+#define true false
+
+using namespace std;
+typedef MYSTD std;
+
+//RANDOM CODE
+
+int Calc()
+{
+    MYSTD::cout << L"hello no need for multibytes" << MYSTD::endl;    
+    return 1;
+}
+
+void MyFunc(int a, int b, int& c)
+{
+    c = max(a, b);   
+}
+
+int main()
+{
+    Calc();
+
+    bool iTrueorFalse = Calc() ? false : true;
+
+    return 0;
+}
